bac08afe8967b711009e73c6b885d2f4c9bacf81
jakobo007 <oumajames>
2024-07-03 11:06:15 -0700

    puppet 4

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,2 +1,12 @@
-#Using puppet to install flask from pip3
-pip3 install flask == 2.1.0
+# Ensure the 'pip3' package provider is available
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask version 2.1.0 using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
